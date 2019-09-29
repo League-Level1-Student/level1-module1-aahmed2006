0da7403a99b86959661ff67a2363e9a7d6747e83
League Student <league@iMac-6.attlocal.net>
2019-09-28 17:26:39 -0700

    Intro to Constructors

---
+++ src/Platypus.java
@@ -0,0 +1,8 @@
+public class Platypus {
+    public String name;
+    void sayHi(){
+        System.out.println("The platypus " + name + " is smarter than your average platypus.");
+    
+    }
+   
+}
